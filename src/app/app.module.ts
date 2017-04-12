4d8a4ed417b38954cec9afcd9ca9ffd30ff628f3
Preston Lamb <preston.j.lamb@gmail.com>
2017-04-12 09:25:38 -0600

    Moved firebaseConfig to its own file and out of source control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { JokesListComponent } from './jokes-list/jokes-list.component';
 
-export const firebaseConfig = {
-	apiKey: "AIzaSyBsX7AR_-MzXuyhM3GiGaXD9PVE7CE13bw",
-	authDomain: "russ-jokes.firebaseapp.com",
-	databaseURL: "https://russ-jokes.firebaseio.com",
-	projectId: "russ-jokes",
-	storageBucket: "russ-jokes.appspot.com",
-	messagingSenderId: "565390903031"
-};
+import { firebaseConfig } from "./firebase.config";
 
 @NgModule({
 	declarations: [
