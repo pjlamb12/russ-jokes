a6e95cba2e8993d912bae30df11349d9df96a989
Preston Lamb <preston.j.lamb@gmail.com>
2017-09-12 00:58:02 -0600

    prettiered the files

--- src/app/jokes-list/jokes-list.component.ts
+++ src/app/jokes-list/jokes-list.component.ts
@@ -1,21 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
-
 @Component({
 	selector: 'app-jokes-list',
 	templateUrl: './jokes-list.component.html',
-	styleUrls: ['./jokes-list.component.css']
+	styleUrls: ['./jokes-list.component.css'],
 })
 export class JokesListComponent implements OnInit {
 	public jokes: FirebaseListObservable<any[]>;
 	constructor(afdb: AngularFireDatabase) {
 		this.jokes = afdb.list('/jokes');
 		console.log(this.jokes);
-		// this.jokes.push({ opening: 'Why did the skunk cross the road?', punchline: 'To get to do odor side.' });
 	}
 
-	ngOnInit() {
-	}
-
+	ngOnInit() {}
 }
